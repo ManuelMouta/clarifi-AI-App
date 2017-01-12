0dbba38f62583e796c98c2c5009339ce482ad012
Manuel Mouta <moutamanuel26@gmail.com>
2017-01-12 23:53:03 +0000

    creating side menu

--- app/src/main/java/manuelmouta/clarifi_ai_app/activities/ReleaseLandingPageActivity.java
+++ app/src/main/java/manuelmouta/clarifi_ai_app/activities/ReleaseLandingPageActivity.java
@@ -2,18 +2,42 @@ package manuelmouta.clarifi_ai_app.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import manuelmouta.clarifi_ai_app.R;
+import yalantis.com.sidemenu.interfaces.Resourceble;
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+import yalantis.com.sidemenu.util.ViewAnimator;
 
 /**
  * Created by manuelmouta on 09/01/17.
  */
 
-public class ReleaseLandingPageActivity extends BaseActivity{
+public class ReleaseLandingPageActivity extends BaseActivity implements ViewAnimator.ViewAnimatorListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.release_landing_page);
+        setContentView(R.layout.train_page);
+    }
+
+    @Override
+    public ScreenShotable onSwitch(Resourceble slideMenuItem, ScreenShotable screenShotable, int position) {
+        return null;
+    }
+
+    @Override
+    public void disableHomeButton() {
+
+    }
+
+    @Override
+    public void enableHomeButton() {
+
+    }
+
+    @Override
+    public void addViewToContainer(View view) {
+
     }
 }

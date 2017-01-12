50b4a0607b392f69b0ec1e14e605729c98647e68
Manuel Mouta <moutamanuel26@gmail.com>
2017-01-12 23:02:41 +0000

    side menu library added

---
+++ app/src/main/java/manuelmouta/clarifi_ai_app/fragments/ContentFragment.java
@@ -0,0 +1,26 @@
+package manuelmouta.clarifi_ai_app.fragments;
+
+import android.graphics.Bitmap;
+import android.support.v4.app.Fragment;
+
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+/**
+ * Created by manuelmouta on 12/01/17.
+ */
+
+public class ContentFragment extends Fragment implements ScreenShotable {
+
+
+
+    
+    @Override
+    public void takeScreenShot() {
+
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return null;
+    }
+}
